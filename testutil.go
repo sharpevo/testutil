2186d7239792e675e33819fc48ae9722013ab5ff
sharpevo <sharpevo@outlook.com>
2020-08-18 10:52:28 +0800

    Add Helper()

--- testutil.go
+++ testutil.go
@@ -1,44 +1,34 @@
 package testutil
 
 import (
-	"fmt"
-	"path/filepath"
 	"reflect"
-	"runtime"
 	"testing"
 )
 
 func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	tb.Helper()
 	if !condition {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
-		tb.FailNow()
+		tb.Fatalf("\033[31m"+msg+"\033[39m\n\n", v...)
 	}
 }
 
-// ok fails the test if an err is not nil.
 func Ok(tb testing.TB, err error) {
+	tb.Helper()
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		tb.FailNow()
+		tb.Fatalf("\033[31munexpected error: %s\033[39m\n\n", err.Error())
 	}
 }
 
-// NotOk fails the test if an err is nil.
 func NotOk(tb testing.TB, err error) {
+	tb.Helper()
 	if err == nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: expected error, got none\033[39m\n\n", filepath.Base(file), line)
-		tb.FailNow()
+		tb.Fatalf("\033[31mexpected error, got none\033[39m\n\n")
 	}
 }
 
-// equals fails the test if exp is not equal to act.
 func Equal(tb testing.TB, exp, act interface{}) {
+	tb.Helper()
 	if !reflect.DeepEqual(exp, act) {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d:\n\tEXPECT: %#v\n\tACTUAL: %#v\033[39m\n", filepath.Base(file), line, exp, act)
-		tb.FailNow()
+		tb.Fatalf("\033[31m\n\tEXPECT: %#v\n\tACTUAL: %#v\033[39m\n", exp, act)
 	}
 }
